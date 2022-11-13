0a9efd9696e3a361fb54176d8918f720f0877865
Jamie Byer <byerjamie@gmail.com>
2022-11-13 14:00:12 -0800

    2d model working now

--- utilities/plotters.py
+++ utilities/plotters.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+from matplotlib import animation
 import xarray as xr
 from utilities.odes import dT_dt, dT_dt_2d
 from scipy.integrate import solve_ivp
@@ -24,68 +24,80 @@ def plot_model():
     T = sol.y.T
 
     plt.plot(t / 3.154E7, T[:, 1:-1])
-    #plt.plot(t / 3.154E7, T[:, 1])
     
     plt.title("radiative balance")
     plt.xlabel("time (yr)")
     plt.ylabel("temperature (K)")
-    #plt.legend(["zone 1", "zone 2", "zone 3", "zone 4", "zone 5", "zone 6"])
+    plt.legend(["zone 1", "zone 2", "zone 3", "zone 4", "zone 5", "zone 6"])
     
     plt.tight_layout()
     plt.show()
 
 def plot_2d_model():
     # Resolution
-    lat_res = 7
-    lon_res = 7
+    lat_res = 50
+    lon_res = 50
 
     # Time interval for integration (in seconds)
     t_min = 0
-    #t_max = 1E11
-    t_max = 1E-5
-    max_step = 1E5# = 0.1#1E6
+    t_max = 1E9
+    max_step = 1E6
 
     # Initial temperatures
     T_init = np.pad(np.full((lat_res-1, lon_res-1), 250), ((1, 1), (1, 1))).flatten()
 
-    sol = solve_ivp(
-        fun=dT_dt_2d, 
-        t_span=(t_min, t_max), 
-        y0=T_init, 
-        method="LSODA", 
-        max_step=max_step, 
-        args=get_2d_constants(lat_res, lon_res)
-        )
+    # Call solver
+    sol = solve_ivp(fun=dT_dt_2d, t_span=(t_min, t_max), y0=T_init, method="LSODA", max_step=max_step, args=get_2d_constants(lat_res, lon_res))
     t = sol.t
     T = sol.y.T
-
-    lats = np.linspace(-90, 90, lat_res)
-    lons = np.linspace(0, 360, lon_res)
-
+    
     T = T.reshape((len(t), lat_res+1, lon_res+1))
 
-    plot_lats = lats[1:] + (lats[1:]-lats[:-1]) / 2
-    plot_lons = lons[1:] + (lons[1:]-lons[:-1]) / 2
-
-    X, Y = np.meshgrid(plot_lats, plot_lons)
-    plt.contourf(X, Y, T[-1, 1:-1, 1:-1])
+    # Time vs. Temperature plot
+    # Loop over each zone to plot
+    for i in range(lat_res-1) :
+        for j in range(lon_res-1):
+            plt.plot(t / 3.154E7, T[:, i+1, j+1])
     
     plt.title("radiative balance")
-    plt.xlabel("lat")
-    plt.ylabel("lon")
+    plt.xlabel("time (yr)")
+    plt.ylabel("temperature (K)")
     
     plt.tight_layout()
     plt.show()
 
-def animate_2d_model():
+    # Map plot. Lat, lon axes.
+    lats = np.linspace(-90, 90, lat_res)
+    lons = np.linspace(0, 360, lon_res)
+
+    plot_lats = lats[:-1] + (lats[1:]-lats[:-1]) / 2
+    plot_lons = lons[:-1] + (lons[1:]-lons[:-1]) / 2
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    div = make_axes_locatable(ax)
+    cax = div.append_axes('right', '5%', '5%')
+
+    vmin, vmax = np.min(T[-1, 1:-1, 1:-1]), np.max(T[-1, 1:-1, 1:-1])
+
+    cf = ax.pcolormesh(plot_lons, plot_lats, T[-1, 1:-1, 1:-1], vmin=vmin, vmax=vmax)
+    cb = fig.colorbar(cf, cax=cax)
+    tx = ax.set_title('t = ' + str(t[-1] / 3.154E7) + " yr")
+    ax.set_ylabel("lat")
+    ax.set_xlabel("lon")
+
+    plt.show()
+
+def animate_2d_model(save=False):
     # Resolution
     lat_res = 50
     lon_res = 50
 
     # Time interval for integration (in seconds)
     t_min = 0
-    t_max = 1E5 #1E11
-    max_step = 1E3 #1E6
+    t_max = 1E9
+    max_step = 1E6
 
     # Initial temperatures
     T_init = np.pad(np.full((lat_res-1, lon_res-1), 250), ((1, 1), (1, 1))).flatten()
@@ -117,7 +129,7 @@ def animate_2d_model():
     div = make_axes_locatable(ax)
     cax = div.append_axes('right', '5%', '5%')
 
-    vmin, vmax = 249.8, 250.2
+    vmin, vmax = np.min(T[-1, 1:-1, 1:-1]), np.max(T[-1, 1:-1, 1:-1])
 
     cf = ax.pcolormesh(plot_lons, plot_lats, T[0, 1:-1, 1:-1], vmin=vmin, vmax=vmax)
     cb = fig.colorbar(cf, cax=cax)
@@ -131,16 +143,31 @@ def animate_2d_model():
         fig.colorbar(cf, cax=cax)
         tx.set_text('t = {0}s'.format(t[i]))
 
-        #plt.title("radiative balance")
-        #ax.set_ylabel("lon")
-        #ax.set_xlabel("lat")    
-        #ax.tight_layout()    
+    anim = animation.FuncAnimation(fig, animate, frames=len(t), interval=100, repeat=False)
 
-    ani = FuncAnimation(fig, animate, frames=len(t), interval=100, repeat=False)
+    if save:
+        f = r"./animation.gif" 
+        writergif = animation.PillowWriter(fps=30) 
+        anim.save(f, writer=writergif)
 
     plt.show()
 
-
+def plot_albedo():
+    lat_res = 50
+    lon_res = 50
+    _, _, _, alpha, _, _, _, _, _ = get_2d_constants(lat_res, lon_res)
+    lats = np.linspace(-90, 90, lat_res)
+    lons = np.linspace(0, 360, lon_res)
+    plot_lats = lats[:-1] + (lats[1:]-lats[:-1]) / 2
+    plot_lons = lons[:-1] + (lons[1:]-lons[:-1]) / 2
+    plt.pcolormesh(plot_lons, plot_lats, alpha[1:-1, 1:-1])
+    cbar = plt.colorbar()
+    cbar.set_label('albedo', rotation=90)
+    plt.title("map of albedo")
+    plt.xlabel("lon")
+    plt.ylabel("lat")
+    plt.tight_layout()
+    plt.show()
 
 def plot_maps():
     land = "sftlf"
--- utilities/write_from_models.py
+++ utilities/write_from_models.py
@@ -3,30 +3,39 @@ import numpy as np
 import csv
 from data.constants import *
 
+# Variable names for datasets.
 land = "sftlf"
 land_ice = "sftgif"
 sea_ice = "siconca"
 grid_area = "areacella"
 
-# Open data for land, land ice, sea ice
+# Open data for land, land ice, sea ice. Average sea ice over one year.
 ds_land = xr.open_zarr("data/models/"+land+".zarr")
 ds_land_ice = xr.open_zarr("data/models/"+land_ice+".zarr")
 ds_sea_ice = xr.open_zarr("data/models/"+sea_ice+".zarr").isel(time=slice(0, 12)).mean(dim="time")
+
+# Area of each cell for the other datasets.
 ds_grid_area = xr.open_zarr("data/models/"+grid_area+".zarr")
 
+# Interpolate sea ice to have same lat lon as other datasets
+ds_sea_ice = ds_sea_ice.interp(lat=ds_grid_area["lat"], lon=ds_grid_area["lon"])
 
 def write_1d_fractions():
+    # Write 1d zonal fractions to a csv.
+
     # Array to store zonal fractions
     f = [[],[],[]]
 
+    # Loop over each zone latitude to add fractions to f
     for i in np.arange(-90, 60+1, 30):
-        zone_area = ds_grid_area.sel(lat=slice(i, i+30))[grid_area].values.mean()
+        # Weigh fractions by area, since area varies over a zone
+        zone_area = ds_grid_area.sel(lat=slice(i, i+30))[grid_area].values
         zone_weighted_area = zone_area / zone_area.mean()
-        #zone_weighted_area = zone_area / ds_grid_area[grid_area].values.mean()
 
-        land_frac = (ds_land*zone_weighted_area).sel(lat=slice(i, i+30))[land].values.mean()
-        land_ice_frac = (ds_land_ice*zone_weighted_area).sel(lat=slice(i, i+30))[land_ice].values.mean()
-        sea_ice_frac = (ds_sea_ice*zone_weighted_area).sel(lat=slice(i, i+30))[sea_ice].values.mean()
+        # Get mean land and ice fractions from datasets
+        land_frac = (zone_weighted_area*ds_land.sel(lat=slice(i, i+30))[land].values).mean()
+        land_ice_frac = (zone_weighted_area*ds_land_ice.sel(lat=slice(i, i+30))[land_ice].values).mean()
+        sea_ice_frac = (zone_weighted_area*ds_sea_ice.sel(lat=slice(i, i+30))[sea_ice].values).mean()
 
         f[0].append((land_frac - land_ice_frac)/100) # land
         f[1].append((100 - land_frac - sea_ice_frac)/100) # ocean
@@ -38,53 +47,71 @@ def write_1d_fractions():
         csvWriter.writerows(np.array(f).T)
 
 def get_2d_constants(lat_res, lon_res):
-    # Array to store zonal fractions
+    # Return constants needed for 2d model
+
+    # Initialize rho_c_Z, alpha arrays.
     rho_c_Z = np.ones((lat_res-1, lon_res-1))
     alpha = np.zeros((lat_res-1, lon_res-1))
 
     lats = np.linspace(-90, 90, lat_res)
     lons = np.linspace(0, 360, lon_res)
 
+    # Loop over each zone.
     for i in range(len(lats)-1):
         for j in range(len(lons)-1):
-            zone_area = ds_grid_area.sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[grid_area].values.mean()
-            #zone_weighted_area = zone_area / zone_area.mean()
-            zone_weighted_area = zone_area / ds_grid_area[grid_area].values.mean()
+            # Get weighting factor based on area across zone.
+            zone_area = ds_grid_area.sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[grid_area].values
+            zone_weighted_area = zone_area / zone_area.mean()
 
-            land_frac = (ds_land*zone_weighted_area).sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[land].values.mean()
-            land_ice_frac = (ds_land_ice*zone_weighted_area).sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[land_ice].values.mean()
-            sea_ice_frac = (ds_sea_ice*zone_weighted_area).sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[sea_ice].values.mean()
+            # Get mean land, ice fractions from datasets
+            land_frac = (zone_weighted_area*ds_land.sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[land].values).mean()
+            land_ice_frac = (zone_weighted_area*ds_land_ice.sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[land_ice].values).mean()
+            sea_ice_frac = (zone_weighted_area*ds_sea_ice.sel(lat=slice(lats[i], lats[i+1]), lon=slice(lons[j], lons[j+1]))[sea_ice].values).mean()
 
             f_land = (land_frac - land_ice_frac)/100
             f_ocean = (100 - land_frac - sea_ice_frac)/100
             f_ice = (land_ice_frac + sea_ice_frac)/100
 
-            rho_c_Z[i, j] = np.dot([f_land, f_ocean, f_ice], LOI_rho_Z_c)
-            alpha[i, j] = np.dot([f_land, f_ocean, f_ice], LOI_alpha)
+            f = np.array([f_land, f_ocean, f_ice])
+            f = f.clip(min=0) # Remove negative values
+
+            if np.sum(f) > 1:
+                # If the total fraction is more than 1, scale it down
+                f = (1/(np.sum(f)))*f
+
+            # Update rho_c_Z, alpha arrays from f values
+            rho_c_Z[i, j] = np.dot(f, LOI_rho_Z_c)
+            alpha[i, j] = np.dot(f, LOI_alpha)
 
 
     rho_c_Z = np.pad(rho_c_Z, ((1, 1), (1, 1)), constant_values=(1, 1))
     alpha = np.pad(alpha, ((1, 1), (1, 1)))
 
+    # Constant value for k
     k = 1E7 # W m-1 K-1
     
+    # Get length of lat, lon boundaries.
     L_lat = np.zeros(lat_res)
     L_lon = np.zeros(lon_res)
 
     L_lat[1:] = 2*np.pi*R_E*np.cos(lats[1:])
     L_lon[1:] = 2*np.pi*R_E*np.cos(lons[1:])
 
+    # Area of each spherical latitudinal zone
     A_sphere_zones = A_E * (1/2)*(np.sin(np.deg2rad(lats[1:])) - np.sin(np.deg2rad(lats[0:-1])))
-    A = A_sphere_zones / lon_res
+    A = A_sphere_zones / lon_res # Area of each zone, split by lat and lon
+    A = np.pad(A, (1, 1))
 
+    # Get area of each circular latitudinal zone
     theta = np.deg2rad(2*(lats + 90))
     A_segments = (R_E**2 / 2) * (theta - np.sin(theta))
 
     A_disk = A_segments[1:] - A_segments[:-1]
 
+    # Calculate gamma for each latitudinal zone
     gamma = np.zeros(lat_res + 1)
     gamma[1:-1] = A_disk / A_sphere_zones
 
-    A = np.pad(A, (1, 1))
-
     return lat_res, lon_res, rho_c_Z, alpha, L_lat, L_lon, k, A, gamma
+
+#write_1d_fractions()
